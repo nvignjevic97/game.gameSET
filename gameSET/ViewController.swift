b58f55434e08540027d6eed177935532a1f268a2
Rade <rade.vignjevic@students.finki.ukim.mk>
2020-05-08 16:14:52 +0200

    Initial Commit

---
+++ gameSET/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gameSET
+//
+//  Created by Rade Vignjevic on 5/8/20.
+//  Copyright © 2020 Nikola Vignjevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
